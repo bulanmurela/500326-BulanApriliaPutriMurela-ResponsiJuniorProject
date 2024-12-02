69a511fdfe7382b7b7b9767c458a1a8247753177
bulanmurela <bulanaputrim@gmail.com>
2024-12-02 08:45:20 +0700

    Add project files.

---
+++ Departemen.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace responsi2junpro
+{
+    internal class Departemen
+    {
+        private int id_dep;
+        private string nama_dep;
+
+        public int ID_dep
+        {
+            get { return id_dep; }
+        }
+
+        public string Nama_Dep
+        {
+            get { return nama_dep; }
+            set { nama_dep = value; }
+        }
+    }
+}
---
+++ Karyawan.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace responsi2junpro
+{
+    internal class Karyawan
+    {
+        private string id_karyawan;
+        private string nama;
+        private int id_dep;
+        private string nama_dep;
+
+        public string ID_Karyawan
+        {
+            get { return id_karyawan; }
+            set { id_karyawan = value; }
+        }
+
+        public string Nama
+        {
+            get { return nama; }
+            set { nama = value; }
+        }
+
+        public int ID_Dep
+        {
+            get { return id_dep; }
+            set { id_dep = value; }
+        }
+
+        public string Nama_Dep
+        {
+            get { return nama_dep; }
+            set { nama_dep = value; }
+        }
+    }
+}
